9505057fd55aa1fd05c3cbd3a58ac8231b3cc9d6
igor <embiggenerd@gmail.com>
2020-05-27 17:27:02 -0400

    state for grid and ability to draw grid

--- aglorithms/gameOfLife.js
+++ aglorithms/gameOfLife.js
@@ -2,9 +2,12 @@
   let template = document.createElement('template');
   template.innerHTML = `
     <style>
-      :host {
-        height: 100%;
-        width: 100%;
+      // :host {
+      //   height: 100%;
+      //   width: 100%;
+      // }
+      canvas{
+        border: black solid 1px;
       }
       
     </style>
@@ -16,28 +19,87 @@
       this.attachShadow({ mode: 'open' });
       this.shadowRoot.appendChild(template.content.cloneNode(true));
       this.$canvas = this.shadowRoot.querySelector('canvas')
-      this.c = this.$canvas.getContext('2d')
+      this.ctx = this.$canvas.getContext('2d')
+      this.animate = this.animate.bind(this)
+      this.x = 100
+      this.y = 100
+      this.iw = 800
+      this.ih = 800
+      this.dx = 4
+      this.dy = 4
+
+      this.radius = 30
     }
     connectedCallback() {
-      this.$canvas.width = window.innerWidth
-      this.$canvas.height = window.innerHeight
-      this.drawRect()
-      this.drawLine()
+      this.$canvas.width = this.iw
+      this.$canvas.height = this.ih
+      this.drawGrid()
+      // this.drawRect()
+      // this.drawLine()
+      // this.animate()
     }
 
-    drawRect(){
+    drawGrid() {
+      const arr = [...Array(100).keys()].map(() => [])
+
+      for (let i = 0; i < 100; i++) {
+        for (let j = 0; j < 100; j++) {
+          arr[i][j] = 0
+        }
+      }
+
+      for (let i = 0; i < 100; i++) {
+        for (let j = 0; j < 100; j++) {
+          this.ctx.fillStyle = "blue"
+          this.ctx.fillRect(i*8, j*8, 7, 7)
+          // this.ctx.stroke()
+        }
+      }
+      
+      console.log(arr)
+    }
+
+    drawRect() {
       const x = 100
       const y = 100
       const width = 100
       const height = 100
-      this.c.fillRect(x, y, width, height)
+      this.c.fillRect(x, y, 1, 1)
+      this.c.stroke()
     }
 
-    drawLine(){
+    drawLine() {
       this.c.beginPath()
       this.c.moveTo(50, 300)
       this.c.lineTo(60, 200)
       this.c.stroke()
     }
+
+    drawCircle() {
+      this.c.beginPath()
+      this.c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+      this.c.strokeStyle = "blue"
+      this.c.stroke()
+    }
+
+    animate() {
+      requestAnimationFrame(this.animate)
+      this.clear()
+      this.drawCircle()
+      if ((this.x > this.iw - this.radius) || (this.x < 0 + this.radius)) {
+        this.dx = -this.dx
+      }
+      if ((this.y > this.ih - this.radius) || (this.y < 0 + this.radius)) {
+        this.dy = -this.dy
+      }
+      this.x += this.dx
+      this.y += this.dy
+      console.log('hihi')
+    }
+
+    clear() {
+      this.c.clearRect(0, 0, this.iw, this.ih)
+    }
+
   });
 })();
